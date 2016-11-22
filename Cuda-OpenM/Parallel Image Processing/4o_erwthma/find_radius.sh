1c5cbd59429b16f2340df9beda7e40c4fa64d2a8
Thanos X <spareme@mail.com>
2016-11-22 20:53:09 +0200

    remove old files

--- Cuda-OpenM/Parallel Image Processing/4o_erwthma/find_radius.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for i in `seq 1 32`;
-do
-	./5o_erwthma_modified_for_script_use << EOF
-$i
-EOF
-	done
--- Cuda-OpenM/Parallel Image Processing/6o_erwthma/find_execution_time_with_steady_filter_DOUBLES.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for i in 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
-do	
-	./6o_erwthma_steady_filter << EOF
-$i
-EOF
-done
